f967b002f756eff233467069f6aec92f10dd2d65
SK_Artemis <98838698+ArK5ZER0@users.noreply.github.com>
2023-10-12 06:22:14 +0200

    Update os-install.js
    update

--- os-install.js
+++ os-install.js
@@ -41,7 +41,7 @@ export async function main(ns) {
             ns.tprint(`WARNING: "${fullLocalFilePath}" was not updated. (Currently running, or not located at ${remoteFilePath}?)`)
     }
     ns.tprint(`[Edit by SK_Artemis] ` +
-              `INFO: Pull complete. `);
+              `INFO: Pull complete. Set alias to this:[ alias -g bootOS="run /os/main.js ] To start, type: bootOS `);
     // Remove any temp files / scripts from the prior version
     ns.run(pathJoin(options.subfolder, `cleanup.js`));
 }
